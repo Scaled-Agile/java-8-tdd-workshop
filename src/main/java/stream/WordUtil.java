d214ae9793bddb892d66084d1ea7cb185701c02a
Yongmin Xia <yongmin.xia@rea-group.com>
2016-11-22 15:03:19 +1100

    Add solution for stream exercises

--- src/main/java/stream/WordUtil.java
+++ src/main/java/stream/WordUtil.java
@@ -1,36 +1,54 @@
 package stream;
 
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.lang.Character.toUpperCase;
 
 public class WordUtil {
 
     // TODO: Implement using Stream API
     public static List<String> upCase(List<String> words) {
-        throw new UnsupportedOperationException("To be implemented");
+        return words.stream().map(String::toUpperCase).collect(Collectors.toList());
     }
 
     // TODO: Implement using Stream API
     public static List<String> upCaseFirst(List<String> words) {
-        throw new UnsupportedOperationException("To be implemented");
+        return words.stream().map(w -> toUpperCase(w.charAt(0)) + w.substring(1)).collect(Collectors.toList());
     }
 
     // TODO: Implement using Stream API
     public static <T> long countBy(List<T> words, Predicate<T> predicate) {
-        throw new UnsupportedOperationException("To be implemented");
+        return words.stream().filter(predicate).count();
     }
 
     public static <T> List<T> zip(List<T> words1, List<T> words2) {
-        throw new UnsupportedOperationException("To be implemented");
+        Stream<T> first = words1.stream();
+        Iterator<T> iterator = words2.stream().iterator();
+
+        Stream<T> result = first.flatMap(
+            t -> iterator.hasNext() ? Arrays.asList(t, iterator.next()).stream() : Arrays.asList(t).stream()
+        );
+
+        if (iterator.hasNext()) {
+            return Stream.concat(result, words2.stream().skip(words1.size())).collect(Collectors.toList());
+        }
+
+        return result.collect(Collectors.toList());
     }
 
     public static int maxLength(List<String> words) {
         // TODO: Implement using Stream reduce
-        throw new UnsupportedOperationException("To be implemented");
+        return words.stream().map(String::length).max(Integer::compare).get();
     }
 
-    public static Map<String,Long> groupBy(List<String> words) {
-        throw new UnsupportedOperationException("To be implemented");
+    public static Map<String, Long> groupBy(List<String> words) {
+        return words.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
     }
 }
